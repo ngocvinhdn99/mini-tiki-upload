9dfe5a0a7a14bd6a51b9d2caef0cd3a8d3ea5a1d
Vinh Huynh <hocdaihoc1999@gmail.com>
2021-11-08 23:40:52 +0700

    update git

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Ngoc Vinh99.
+          Edit <code>src/App.js</code> and save to reload by Ngoc Vinh99 .
         </p>
+        <h2>Lionel Messi</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
